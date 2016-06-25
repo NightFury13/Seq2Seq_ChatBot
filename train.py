eba19da068495f11da7e61b7e6f030eaf47a981b
NightFury13 <develop13mohit@gmail.com>
2016-06-25 18:30:53 +0530

    Release 1.0 : Char-Level ChatBot Now Trains

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env
 
 from __future__ import print_function
 from keras.models import Sequential
@@ -43,93 +44,65 @@ class colors:
     close = '\033[0m'
 
 #------- Global Variables ---------#
-gen_vocab_filepath = 'new_data/joined_data.txt'
-gen_vocab_outpath  = 'vocab.pickle'
-content_filepath   = 'new_data/out_assassinscreed.tsv'
+content_filepath   = 'new_data/out_TheSimpsons.tsv'
+chars = '0123456789+/-*=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,?(){}[]&#_ ' # Char-Level Vocabulary.
+TRUNCATE_SIZE = 20000
 #----------------------------------#
 
-#--------- Load the gen-vocab ----------#
-gen_vocab = []
-if not os.path.isfile(gen_vocab_outpath):
-    # Create the vocab
-    gen_vocab = genVocabCreator(gen_vocab_filepath)
-    gen_vocab.saveVocabToFile(gen_vocab_outpath)
-#---------------------------------------#
-
 #--------- Load the context-response pairs -----------#
-dataset = dataLoader(content_filepath, gen_vocab_outpath) # access data as dataset.contexts, dataset.responses
+dataset = dataLoader(content_filepath) # access data as dataset.contexts, dataset.responses
+print("[Data-Loader] : Loaded", len(dataset.contexts), "context-response pairs")
 #-----------------------------------------------------#
 
+#-------- Parameters for the model and dataset--------#
+questions = dataset.contexts[:TRUNCATE_SIZE]
+expected = dataset.responses[:TRUNCATE_SIZE]
 
-# Parameters for the model and dataset
-TRAINING_SIZE = 50000
-DIGITS = 3
-INVERT = True
-# Try replacing GRU, or SimpleRNN
 RNN = recurrent.LSTM
-HIDDEN_SIZE = 128
+HIDDEN_SIZE = 512
 BATCH_SIZE = 128
-LAYERS = 1
-MAXLEN = DIGITS + 1 + DIGITS
+LAYERS = 3
+X_MAXLEN = len(max(questions, key=len))
+Y_MAXLEN = len(max(expected, key=len))
 
-chars = '0123456789+ '
-ctable = CharacterTable(chars, MAXLEN)
+ctable = CharacterTable(chars, X_MAXLEN)
+#-----------------------------------------------------#
 
-questions = []
-expected = []
-seen = set()
-print('Generating data...')
-while len(questions) < TRAINING_SIZE:
-    f = lambda: int(''.join(np.random.choice(list('0123456789')) for i in range(np.random.randint(1, DIGITS + 1))))
-    a, b = f(), f()
-    # Skip any addition questions we've already seen
-    # Also skip any such that X+Y == Y+X (hence the sorting)
-    key = tuple(sorted((a, b)))
-    if key in seen:
-        continue
-    seen.add(key)
-    # Pad the data with spaces such that it is always MAXLEN
-    q = '{}+{}'.format(a, b)
-    query = q + ' ' * (MAXLEN - len(q))
-    ans = str(a + b)
-    # Answers can be of maximum size DIGITS + 1
-    ans += ' ' * (DIGITS + 1 - len(ans))
-    if INVERT:
-        query = query[::-1]
-    questions.append(query)
-    expected.append(ans)
-print('Total addition questions:', len(questions))
-
-print('Vectorization...')
-X = np.zeros((len(questions), MAXLEN, len(chars)), dtype=np.bool)
-y = np.zeros((len(questions), DIGITS + 1, len(chars)), dtype=np.bool)
+#---------- Pre-process the data ---------------------#
+print('[Pre-Process] : Performing data vectorization...')
+X = np.zeros((len(questions), X_MAXLEN, len(chars)), dtype=np.bool)
+y = np.zeros((len(questions), Y_MAXLEN, len(chars)), dtype=np.bool)
 for i, sentence in enumerate(questions):
-    X[i] = ctable.encode(sentence, maxlen=MAXLEN)
+    X[i] = ctable.encode(sentence, maxlen=X_MAXLEN)
 for i, sentence in enumerate(expected):
-    y[i] = ctable.encode(sentence, maxlen=DIGITS + 1)
+    y[i] = ctable.encode(sentence, maxlen=Y_MAXLEN)
 
 # Shuffle (X, y) in unison as the later parts of X will almost all be larger digits
+print('[Pre-Process] : Shuffling the data')
 indices = np.arange(len(y))
 np.random.shuffle(indices)
 X = X[indices]
 y = y[indices]
 
 # Explicitly set apart 10% for validation data that we never train over
+print('[Pre-Process] : Setting 10% data aside as validation-set')
 split_at = len(X) - len(X) / 10
 (X_train, X_val) = (slice_X(X, 0, split_at), slice_X(X, split_at))
 (y_train, y_val) = (y[:split_at], y[split_at:])
 
-print(X_train.shape)
-print(y_train.shape)
+print("Question-Set Shape:",X_train.shape)
+print("Response-Set Shape:",y_train.shape)
+#-----------------------------------------------------#
 
+#-------------- Define the Model ---------------------#
 print('Build model...')
 model = Sequential()
 # "Encode" the input sequence using an RNN, producing an output of HIDDEN_SIZE
 # note: in a situation where your input sequences have a variable length,
 # use input_shape=(None, nb_feature).
-model.add(RNN(HIDDEN_SIZE, input_shape=(MAXLEN, len(chars))))
+model.add(RNN(HIDDEN_SIZE, input_shape=(X_MAXLEN, len(chars))))
 # For the decoder's input, we repeat the encoded input for each time step
-model.add(RepeatVector(DIGITS + 1))
+model.add(RepeatVector(Y_MAXLEN))
 # The decoder RNN could be multiple layers stacked or a single layer
 for _ in range(LAYERS):
     model.add(RNN(HIDDEN_SIZE, return_sequences=True))
@@ -137,7 +110,9 @@ for _ in range(LAYERS):
 # For each of step of the output sequence, decide which character should be chosen
 model.add(TimeDistributed(Dense(len(chars))))
 model.add(Activation('softmax'))
+#-----------------------------------------------------#
 
+#--------------- Train the Model ---------------------#
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
@@ -162,3 +137,4 @@ for iteration in range(1, 200):
         print('T', correct)
         print(colors.ok + '☑' + colors.close if correct == guess else colors.fail + '☒' + colors.close, guess)
         print('---')
+#-----------------------------------------------------#
